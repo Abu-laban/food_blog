70d3a55f86d4880776ac022c51eb8bdcf2e19d45
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-06-12 14:12:41 +0300

    finish Template

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
-"use strict";
+'use strict';
+
+let sideNav = document.getElementById('sideBar');
+
+let btnEl1 = document.getElementById('btn1');
+
+let btnEl2 = document.getElementById('btn2');
+
+let btnEl3 = document.getElementById('btn3');
+
+let btnEl4 = document.getElementById('btn4');
+
+btnEl1.href = 'javascript:void(0)';
+
+btnEl1.onclick = function closeNav() {
+
+    sideNav.style.display = 'none';
+
+};
+
+btnEl2.href = '#food';
+
+btnEl2.onclick = function closeNav() {
+
+    sideNav.style.display = 'none';
+
+};
+
+btnEl3.href = '#about';
+
+btnEl3.onclick = function closeNav() {
+
+    sideNav.style.display = 'none';
+
+};
+
+btnEl4.onclick = function openNav() {
+
+    sideNav.style.display = 'block';
+};
+
